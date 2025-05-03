c1651213216d313a98d9881897da7430a89b9289
Fareed Kulumba <farkane161@gmail.com>
2025-05-03 06:04:25 -0400

    n/a

--- Develop/client/vite.config.ts
+++ Develop/client/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
@@ -14,8 +16,13 @@ export default defineConfig({
       '/auth': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-        secure: false
+        secure: false,
       },
     },
+  },
+  preview: {
+    host: '0.0.0.0', // Ensures the app binds to all network interfaces
+    port: 3000, // Default port for local testing
+    allowedHosts: ['kanban-board-9.onrender.com'], // Add your Render host here
   },
 });
